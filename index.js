b79801283db944f74a60543004438bf1fdf74fa4
Eylem Aytas <eylem.aytas@hotmail.com>
2023-03-12 22:30:36 -0400

    Adds package-lock.json

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b) {
+return "something"
+}
+function add(a, b) {
+return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+number=7;
+function add3(){
+    return (number +=3);
+}
+function divide2(){
+    return (number/=2)
+}
+add3();
+divide2();
+let n = 0;
+function increment(n){
+    n++;
+    return n;
+}
+let n1 =0;
+function decrement(n1) {
+    n1--;
+    return n1;
+}
+function makeInt(n2){
+    return parseInt(n,10)
+}
+function preserveDecimal(n3){
+    return parseFloat(n3)
+}
+function makeInt(n4){
+    return parseInt(n4,10)
+}
